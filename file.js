487d791e4c7ecae4ec4f5cee0f0631b43b304a28
Вероніка bc2024-3 <veronika.korol2020@gmail.com>
2024-10-20 23:53:50 +0300

    Внесено зміни до файлу

--- file.js
+++ file.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const { Command } = require('commander');
 const program = new Command();
 
@@ -12,6 +13,7 @@ program.parse(process.argv);
 
 const options = program.opts();
 
+const inputFilePath = path.resolve(options.input);
 
 if (!options.input) {
   console.error('Please, specify input file');
@@ -27,4 +29,41 @@ if (!fs.existsSync(options.input)) {
 
 if (!options.output && !options.display) {
   process.exit(0);
+}
+
+
+if (options.display) {
+  console.log(result);
+}
+
+if (options.output) {
+  fs.writeFileSync(outputFilePath, result);
+}
+
+const outputFilePath = options.output ? path.resolve(options.output) : 'output.txt';
+
+if (options.output) {
+  try {
+    fs.writeFileSync(outputFilePath, result);
+    console.log(`Result saved to ${outputFilePath}`);
+  } catch (err) {
+    console.error(`Error writing to file: ${err.message}`);
+  }
+}
+
+
+let minAsset = inputData['reserves'].reduce((min, asset) => {
+  return asset.value < min.value ? asset : min;
+}, inputData['reserves'][0]);
+
+
+const result = `${minAsset.name}:${minAsset.value}`;
+
+
+let inputData;
+try {
+  inputData = JSON.parse(fs.readFileSync(inputFilePath, 'utf-8'));
+} catch (error) {
+  console.error('Error reading or parsing input file');
+  process.exit(1);
 }
